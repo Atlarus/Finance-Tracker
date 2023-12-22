c0d162dd2d33c88762dc648c36bf6aa9d7eacd7f
Aliasz1 <mzakwanas@protonmail.com>
2023-12-22 23:44:49 +0800

    Update invoice feature

--- src/pages/InvoicePage.jsx
+++ src/pages/InvoicePage.jsx
@@ -8,7 +8,7 @@ const InvoicePage = ({ invoices, setInvoiceData }) => {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Invoice Page</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <div className="grid gap-4">
         <div>
           <InvoiceForm />
         </div>
--- src/pages/components/actions/InvoiceTracker.jsx
+++ src/pages/components/actions/InvoiceTracker.jsx
@@ -1,18 +1,62 @@
 import React, { useState } from 'react';
 
 const InvoiceTracker = ({ invoices, setInvoiceData }) => {
-  const [trackedInvoices, setTrackedInvoices] = useState(invoices || []);
+  const [editingId, setEditingId] = useState(null);
+  const [newInvoice, setNewInvoice] = useState({
+    companyName: '',
+    clientName: '',
+    invoiceNumber: '',
+    date: '',
+    dueDate: '',
+    amount: '',
+    status: ''
+  })
 
-  const addInvoice = (invoice) => {
-    setTrackedInvoices([...trackedInvoices, invoice]);
+  const statuses = ['paid', 'pending', 'overdue', 'voided'];
+
+  const handleAddInvoice = () => {
+    setInvoiceData([...invoices, newInvoice]);
+    setNewInvoice({
+      companyName: '',
+      clientName: '',
+      invoiceNumber: '',
+      date: '',
+      dueDate: '',
+      amount: '',
+      status: ''
+    })
+  };
+
+  const handleEditInvoice = (invoiceNum) => {
+    setEditingId(invoiceNum);
+    const invoiceToEdit = invoices.find((invoice) => invoice.invoiceNumber === invoiceNum);
+    setNewInvoice({...invoiceToEdit});
+  }
+
+  const handleUpdateInvoice = () => {
+    setInvoiceData(invoices.map((invoice) => (invoice.invoiceNumber === editingId ? newInvoice : invoice)));
+    setEditingId(null);
+    setNewInvoice({
+      companyName: '',
+      clientName: '',
+      invoiceNumber: '',
+      date: '',
+      dueDate: '',
+      amount: '',
+      status: ''
+    })
+  };
+
+  const handleRemoveInvoice = (invoiceNum) => {
+    setInvoiceData(invoices.filter((invoice) => invoice.invoiceNumber !== invoiceNum));
   };
 
   return (
     <div className="bg-white p-4 rounded shadow">
-      <h2 className="text-lg font-semibold mb-4">Invoice Tracker</h2>
+      <h2 className="text-lg font-semibold mb-4">Invoice Issued</h2>
 
       {/* Display invoices in a table */}
-      {trackedInvoices.length > 0 ? (
+      {invoices.length > 0 ? (
         <table className="table-auto w-full">
           <thead>
             <tr>
@@ -24,19 +68,97 @@ const InvoiceTracker = ({ invoices, setInvoiceData }) => {
               <th className="border px-4 py-2">Due Date</th>
               <th className="border px-4 py-2">Amount</th>
               <th className="border px-4 py-2">Status</th>
+              <th className="border px-4 py-2">Actions</th>
             </tr>
           </thead>
           <tbody>
-            {trackedInvoices.map((invoice, index) => (
+            {invoices.map((invoice, index) => (
               <tr key={index}>
                 <td className="border px-4 py-2">{index + 1}</td>
-                <td className="border px-4 py-2">{invoice.companyName}</td>
-                <td className="border px-4 py-2">{invoice.clientName}</td>
-                <td className="border px-4 py-2">{invoice.invoiceNumber}</td>
-                <td className="border px-4 py-2">{invoice.date}</td>
+                <td className="border px-4 py-2">
+                  {editingId === invoice.invoiceNumber ? (
+                    <input
+                      type="text"
+                      value={newInvoice.companyName}
+                      onChange={(e) => setNewInvoice({...newInvoice, companyName: e.target.value })}
+                      className="mt-1 p-2 border rounded-md w-full"
+                      />
+                    ) : (
+                      invoice.companyName
+                    )}
+                </td>
+                <td className="border px-4 py-2">
+                  {editingId === invoice.invoiceNumber ? (
+                    <input
+                      type="text"
+                      value={newInvoice.clientName}
+                      onChange={(e) => setNewInvoice({...newInvoice, clientName: e.target.value })}
+                      className="mt-1 p-2 border rounded-md w-full"
+                      />
+                    ) : (
+                      invoice.clientName
+                    )}
+                </td>
+                <td className="border px-4 py-2">
+                  {editingId === invoice.invoiceNumber ? (
+                    <input
+                      type="text"
+                      value={newInvoice.invoiceNumber}
+                      onChange={(e) => setNewInvoice({...newInvoice, invoiceNumber: e.target.value })}
+                      className="mt-1 p-2 border rounded-md w-full"
+                      />
+                    ) : (
+                      invoice.invoiceNumber
+                    )}
+                </td>
+                <td className="border px-4 py-2">
+                  {editingId === invoice.invoiceNumber ? (
+                    <input
+                      type="date"
+                      value={newInvoice.date}
+                      onChange={(e) => setNewInvoice({...newInvoice, date: e.target.value })}
+                      className="mt-1 p-2 border rounded-md w-full"
+                      />
+                    ) : (
+                      invoice.date
+                    )}
+                </td>
                 <td className="border px-4 py-2">{invoice.dueDate}</td>
                 <td className="border px-4 py-2">{invoice.amount}</td>
                 <td className="border px-4 py-2">{invoice.status}</td>
+                <td className="py-2 px-4 border-b">
+                  {editingId === invoice.invoiceNumber ? (
+                    <div className="flex space-x-2">
+                      <button
+                        onClick={handleUpdateInvoice}
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700"
+                      >
+                        Update
+                      </button>
+                      <button
+                        onClick={() => setEditingId(null)}
+                        className="bg-gray-500 text-white py-2 px-4 rounded-md hover:bg-gray-700"
+                      >
+                        Cancel
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="flex space-x-2">
+                      <button
+                        onClick={() => handleEditInvoice(invoice.invoiceNumber)}
+                        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-700"
+                      >
+                        Edit
+                      </button>
+                      <button
+                        onClick={() => handleRemoveInvoice(invoice.invoiceNumber)}
+                        className="bg-red-500 text-white py-2 px-4 rounded-md hover:bg-red-700"
+                      >
+                        Delete
+                      </button>
+                    </div>
+                  )}
+                </td>
               </tr>
             ))}
           </tbody>
