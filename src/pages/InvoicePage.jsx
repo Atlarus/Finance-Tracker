6283758c86d053f0329b71774102d8cbff7dd289
Aliasz1 <mzakwanas@protonmail.com>
2023-12-23 01:45:54 +0800

    Update InvoicePage.jsx

--- src/pages/InvoicePage.jsx
+++ src/pages/InvoicePage.jsx
@@ -10,7 +10,7 @@ const InvoicePage = ({ invoices, setInvoiceData }) => {
       <h1 className="text-2xl font-bold mb-4">Invoice Page</h1>
       <div className="grid gap-4">
         <div>
-          <InvoiceForm />
+          <InvoiceForm setInvoiceData={setInvoiceData}/>
         </div>
         <div>
           <InvoiceTracker invoices={invoices} setInvoiceData={setInvoiceData} />
