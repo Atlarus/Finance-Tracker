07673bfe22329bee627b9890917367a23497ae7a
Aliasz1 <mzakwanas@protonmail.com>
2024-09-07 01:01:29 +0800

    Removed all files to remake project

--- README.md
+++
@@ -1,1 +0,0 @@
-Finance Tracking System developed by Atlarus Enterprise
--- server/routes/ledger.mjs
+++
@@ -1,7 +0,0 @@
-// Get a list of 50 posts
-router.get("/", async (req, res) => {
-    let collection = await db.collection("Companies");
-    let results = await collection.find({})
-      .toArray();
-    res.send(results).status(200);
-  });
